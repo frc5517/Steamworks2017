a73bae642aad4b7e975e2ba3cd05406d8c3250e7
Nathaniel Johnson <nathancj224@gmail.com>
2017-02-14 01:56:58 -0500

    Set matchStarted to true in teleopInit

--- src/org/usfirst/frc/team5517/robot/Robot.java
+++ src/org/usfirst/frc/team5517/robot/Robot.java
@@ -118,6 +118,8 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopInit() {
+		matchStarted = true;
+		
 		// This makes sure that the autonomous stops running when
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
