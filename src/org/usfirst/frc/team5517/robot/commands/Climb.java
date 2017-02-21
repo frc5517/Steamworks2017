3cfb0d224fb333e10e63e9ff57999d41c9e02c90
Nathan Johnson <nathancj224@gmail.com>
2017-02-21 15:18:30 -0500

    Climber stop/interrupt added

--- src/org/usfirst/frc/team5517/robot/commands/Climb.java
+++ src/org/usfirst/frc/team5517/robot/commands/Climb.java
@@ -38,11 +38,13 @@ public class Climb extends Command {
     // Called once after isFinished returns true
     @Override
 	protected void end() {
+    	Robot.climber.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     @Override
 	protected void interrupted() {
+    	end();
     }
 }
